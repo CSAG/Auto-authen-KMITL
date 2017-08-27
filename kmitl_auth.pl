a7561a4b0b09fe10a7aa472073eac1e6f3fc5ef8
Komphet <komphet@kmi.tl>
2017-08-28 05:03:27 +0700

    update fill input

--- kmitl_auth.pl
+++ kmitl_auth.pl
@@ -1,23 +1,12 @@
 use LWP::UserAgent;
 use HTTP::Cookies;
 use Term::ReadKey;
+use Data::Dumper;
+use JSON;
 
-print "Username: ";
-$username=<>;
-print "Password: ";
-ReadMode( "noecho");
-chomp (my $password = <>);
-ReadMode ("original") ;
-system("clear");
-
-print " ____  __   __   ____  ____      _      ____   ____    ___   _  _____   \n";
-print "| __ ) \\ \\ / /  / ___|/ ___|    / \\    / ___| |___ \\  / _ \\ / ||___  | \n";
-print "|  _ \\  \\ V /  | |    \\___ \\   / _ \\  | |  _    __) || | | || |   / /  \n";
-print "| |_) |  | |   | |___  ___) | / ___ \\ | |_| |  / __/ | |_| || |  / /   \n";
-print "|____/   |_|    \\____||____/ /_/   \\_\\ \\____| |_____| \\___/ |_| /_/    \n";
-print "\n";
-print "\n";
-exit unless($username && $password);
+$username="";
+$password="";
+$isLogin=0;
 
 %ssl_opts=(
 	verify_hostname => 0,
@@ -31,26 +20,48 @@ $agent=LWP::UserAgent->new(
 	max_redirect => 0,
 	cookie_jar => $cookie_jar
 );
-$count = 1;
+$json=JSON->new->allow_nonref;
+
 while(1) {
 	$time=localtime;
-	$checkConnection = checkConnection();
-	if($checkConnection == 1) {
+	$checkConnection=checkConnection();
+	if($checkConnection==1) {
 		print "[$time] Connection OK...\n";
 	}else {
-		print "[$time] Connection Heartbeat!!!\n";
-		login();
+		print "[$time] Connection Reset!!!\n";
+		if(login()==0){
+			sleep 10;
+			next;
+		};
 	}
-	if($count >= 15) {
-		login();
-		$count = 1;
-	}
+	
 	sleep 60;
-	$count++;
+	
 }
 
 sub login {
-		$content=$agent->post('https://10.252.23.2:8445/PortalServer/Webauth/webAuthAction!login.action',[
+	if($username=="" || $username==""){
+		system("cls");
+
+		print " ____  __   __   ____  ____      _      ____   ____    ___   _  _____   \n";
+		print "| __ ) \\ \\ / /  / ___|/ ___|    / \\    / ___| |___ \\  / _ \\ / ||___  | \n";
+		print "|  _ \\  \\ V /  | |    \\___ \\   / _ \\  | |  _    __) || | | || |   / /  \n";
+		print "| |_) |  | |   | |___  ___) | / ___ \\ | |_| |  / __/ | |_| || |  / /   \n";
+		print "|____/   |_|    \\____||____/ /_/   \\_\\ \\____| |_____| \\___/ |_| /_/    \n";
+		print "\n";
+		print "Version fill input by Komphet.me\n";
+		print "\n";
+		print "\n";
+
+		print "Username: ";
+		chomp ($username=<>);
+		print "Password: ";
+		ReadMode( "noecho");
+		chomp ($password=<>);
+		ReadMode ("original") ;
+		print "\nChecking...\n";
+	}
+	$content=$agent->post('https://10.252.23.2:8445/PortalServer/Webauth/webAuthAction!login.action',[
 		'userName' => $username,
 		'password' => $password,
 		'validCode' => '',
@@ -58,16 +69,35 @@ sub login {
 		'hasValidateNextUpdatePassword' => 'true',
 		'browserFlag' => 'en',
 		'ClientIp' => ''
-	])->as_string;
-	print " Finish Sign in\n\n";
+	]);
+
+	if($content->is_success){
+		$check=$agent->get('http://188.166.177.132/check/');
+		if($check->is_success){
+			if($check->as_string=~/Hello planet!\n/){
+				print " You are Loged in!\n\n";
+				return 1;
+			} else {
+				$username="";
+				$password="";
+				print " Username or Password is incorect!\n\n";
+				return 0;
+			}
+		}
+	} else {
+		print " Connection Error!\n\n";
+		return 0;
+	}
+	
 }
+
 sub checkConnection {
-	$content = $agent->get('http://188.166.177.132/check/')->as_string;
-	if($content=~/nac\.kmitl\.ac\.th/) {
-		return 'nac.kmitl.ac.th';
-	}
-	elsif($content=~/Hello planet!\n/) {
-		return 1;
-	}
-	return $content;
+	$check=$agent->get('http://188.166.177.132/check/');
+		if($check->is_success){
+			if($check->as_string=~/Hello planet!\n/){
+				return 1;
+			} else {
+				return 0;
+			}
+		}
 }
